1c1f4566432ad60fd63af70019434b32786cf48c
LM1997610 <115407939+LM1997610@users.noreply.github.com>
2024-07-06 14:42:49 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ def build_test_set(one_serie, compute_features):
  
 
 
-def save_results(datadict, filename):
+def save_results(datadict, filename, output_dir='submissions/'):
 
   results_df = pd.DataFrame(datadict)
 
@@ -25,8 +25,34 @@ def save_results(datadict, filename):
   results_df = results_df.drop(columns=cols_to_drop)
   results_df['volume_kg']  = volumes 
 
-  results_df.to_csv(f'{filename}.csv', index=False)
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+    
+  results_df.to_csv(f'output_dir+{filename}.csv', index=False)
 
   print(f'>> Saved {filename+".csv":>21} - shape: {results_df.shape} - {list(results_df.columns)}')
 
   return results_df
+
+
+def prediction_plot(this_serie, this_pred, this_serie_name, output_dir = 'plots'):
+
+  plt.figure(figsize=(8, 3.5))
+
+  plt.plot(this_serie['volume_kg'])
+  plt.plot(this_pred['volume_kg'], color='r', marker=".", markersize=7, alpha=0.7)
+
+  plt.title('\n'+serie_names[indx]+'\n')
+
+  plt.legend(['Truth', 'Predictions'], loc='upper right')
+  plt.ylabel('\n volume_kg \n', fontsize=11)
+
+  plt.grid('-', alpha=0.5)
+  plt.xticks(rotation=45)
+  plt.tight_layout()
+
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+  plt.savefig(output_dir + f'/{this_serie_name}.png')
+  plt.close()
